60069ca01cd14eff18d761a5b3353af42ede85a1
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-01-28 17:49:24 +0330

    Update models.py
    Define user role

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('M', 'منشی'),
+        ('P', 'بیمار'),
+    )
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+  

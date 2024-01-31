4ff47f90201bad2eb697a0f951ef4dc5397bd6dc
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-01-31 19:12:30 +0330

    Update forms.py
    Add class signup for connect views.py and signup.html

--- blog/forms.py
+++ blog/forms.py
@@ -1,1 +1,7 @@
+from django import forms
 
+class SignupForm(forms.Form):
+    username = forms.CharField(label='نام کاربری')
+    password = forms.CharField(label='رمز عبور', widget=forms.PasswordInput)
+    email = forms.EmailField(label='ایمیل')
+    user_role = forms.ChoiceField(choices=[('patient', 'بیمار'), ('secretary', 'منشی')], label='نقش کاربر')

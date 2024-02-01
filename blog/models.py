e8569b9c1b38333333c3f71e8242cb8c947d670f
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-02-01 12:55:42 +0330

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,48 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .validators import *
 
-class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('M', 'منشی'),
-        ('P', 'بیمار'),
+
+class Patient(models.Model):
+    user_type = models.CharField(
+        max_length=10,
+        choices=[('patient', 'Patient'), ('secretary', 'Secretary')],
+        default='patient'
     )
-    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
-  
+    username = models.CharField(
+            max_length = 20,
+            unique = True,
+            validators = [username_validators]
+        )
+    password = models.CharField(
+            max_length = 50,
+            unique = False,
+            validators = [password_validators]
+        )
+    email = models.EmailField(
+            max_length = 254,
+            blank = True,
+            null = True
+        )
+   
+   
+class Secretary(models.Model):
+    user_type = models.CharField(
+        max_length=10,
+        choices=[('patient', 'Patient'), ('secretary', 'Secretary')],
+        default='patient'
+    )
+    username = models.CharField(
+            max_length = 20,
+            unique = True,
+            validators = [username_validators]
+        )
+    password = models.CharField(
+            max_length = 50,
+            unique = False,
+            validators = [password_validators]
+        )
+    email = models.EmailField(
+            max_length = 254,
+            blank = True,
+            null = True
+        )

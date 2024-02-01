416427be57226611aa8584d6af8c15c7f638fd49
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-02-01 12:54:42 +0330

    Update forms.py
    Add class Loginform for connect views.py and login.html

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,8 @@ class SignupForm(forms.Form):
     password = forms.CharField(label='رمز عبور', widget=forms.PasswordInput)
     email = forms.EmailField(label='ایمیل')
     user_role = forms.ChoiceField(choices=[('patient', 'بیمار'), ('secretary', 'منشی')], label='نقش کاربر')
+    
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='نام کاربری')
+    password = forms.CharField(label='رمز عبور', widget=forms.PasswordInput)

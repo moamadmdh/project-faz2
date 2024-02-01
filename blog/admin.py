083d78eebe08b5aa4d1db8d35c9455b1c471890f
mobin <mobinmotiei@gmail.com>
2024-02-01 17:02:52 +0330

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserModel, Appointment, ClinicModel
 
-# Register your models here.
+admin.site.register(UserModel)
+admin.site.register(Appointment)
+admin.site.register(ClinicModel)

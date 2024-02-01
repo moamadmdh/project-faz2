12878a4634877fe3111f1321c3e180f88864f486
mobin <mobinmotiei@gmail.com>
2024-02-01 22:28:31 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -7,13 +7,25 @@ from .forms import SignupForm
 def user_login(request):
     username = request.POST['username']
     password = request.POST['password']
+    print("username")
+    print("password")
     try:
         patient = Patient.objects.get(username=username)
         if patient.password == password:
             # Login successful, redirect patient to their page
             return redirect('patient_page')
-    except Patient.DoesNotExist:
-        pass
+    except:
+          return redirect('Wrong username or password')
+        if user is not None:
+            print("null")
+            request.session['username'] = username
+            return redirect('Welcome')
+        else:
+            return redirect('Wrong username or password')
+    else:
+        print("nothing")
+        return redirect('Method not allowed')
+
 
     try: 
         secretary = Secretary.objects.get(username=username)
@@ -51,3 +63,32 @@ def user_signup(request):
         form = SignupForm()
 
     return render(request, 'signup.html', {'form': form})
+def add_clinic(request):
+    if request.method == 'POST':
+        clinic_id = request.POST['clinic_id']
+        name = request.POST['name']
+        email = request.POST['email']
+        address = request.POST['address']
+        phone_number = request.POST['phone_number']
+        services = request.POST['services']
+        try:
+            user = ClinicModel.objects.get(email=email)
+            return redirect('/login?msg=dup email')
+        except:
+            pass
+        try:
+            user = ClinicModel.objects.get(clinic_id=clinic_id)
+            return redirect('2 clinic id')
+        except:
+            pass
+        clinic = ClinicModel(clinic_id=clinic_id, email=email,name=name,address=address,phone_number=phone_number, services=services)
+        clinic.save()
+        return redirect('/panel/')
+    else:
+        return redirect('method not allowed')
+def check_log_or_main(request):
+    if 'username' in request.session:
+        return redirect('/panel/')
+    else:
+        return redirect('Please login')
+

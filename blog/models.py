d61fe4939c1ad79e8c5128f225f5dc1be108cdf4
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-02-02 08:53:46 +0330

    Update models.py
    Transforming the patient and secretary class into a user class and defining a new class for the clinic

--- blog/models.py
+++ blog/models.py
@@ -1,48 +1,24 @@
 from django.db import models
 from .validators import *
-
-
-class Patient(models.Model):
-    user_type = models.CharField(
-        max_length=10,
-        choices=[('patient', 'Patient'), ('secretary', 'Secretary')],
-        default='patient'
-    )
-    username = models.CharField(
-            max_length = 20,
-            unique = True,
-            validators = [username_validators]
-        )
-    password = models.CharField(
-            max_length = 50,
-            unique = False,
-            validators = [password_validators]
-        )
-    email = models.EmailField(
-            max_length = 254,
-            blank = True,
-            null = True
-        )
    
-   
-class Secretary(models.Model):
-    user_type = models.CharField(
-        max_length=10,
-        choices=[('patient', 'Patient'), ('secretary', 'Secretary')],
-        default='patient'
+class User(models.Model):
+    user_type = models.CharField(max_length=10, choices=[('patient', 'Patient'), ('secretary', 'Secretary')], default='patient'
     )
-    username = models.CharField(
-            max_length = 20,
-            unique = True,
-            validators = [username_validators]
-        )
-    password = models.CharField(
-            max_length = 50,
-            unique = False,
-            validators = [password_validators]
-        )
-    email = models.EmailField(
-            max_length = 254,
-            blank = True,
-            null = True
-        )
+    username = models.CharField(max_length = 20, unique = True, validators = [username_validators]
+    )
+    password = models.CharField(max_length = 50, unique = False, validators = [password_validators]
+    )
+    email = models.EmailField(max_length = 254, blank = True, null = True
+    )
+    
+class ClinicModel(models.Model):
+    clinic_id = models.CharField(max_length=255, unique=True
+    )
+    name = models.CharField(max_length=255
+    )
+    address = models.CharField(max_length=255
+    )
+    contact_info = models.CharField(max_length=255, unique=True
+    )
+    services = models.CharField(max_length=255,null=True
+    )

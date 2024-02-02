b5a9447176e59182cf41275999dbdd0de28cc7d4
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-02-02 09:13:56 +0330

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import UserModel, Appointment, ClinicModel
+from .models import User, Appointment, ClinicModel
 
-admin.site.register(UserModel)
+admin.site.register(User)
 admin.site.register(Appointment)
 admin.site.register(ClinicModel)

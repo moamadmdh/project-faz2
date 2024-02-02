23f40d3140724defe02ae2acf61ce024434ab80d
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-02-02 22:42:15 +0330

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('login/', views.login, name='login'),
     path('user_login', views.user_login, name='user_login'),
     path('Bimar_dashboard/', views.Bimar_dashboard, name='Bimar_dashboard'),
     path('Monshi_dashboard/', views.Monshi_dashboard, name='Monshi_dashboard'),
     path('add_clinic/', views.add_clinic, name='add_clinic'),
-    
     path('signup.html/', views.signup , name='signup.html'),
-    path('login/signup.html/login.html', views.login, name= 'login.html')
-    
+    path('login/signup.html/login.html', views.login, name= 'login.html')   
 ]

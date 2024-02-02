0e58f389211a59b6941c197f1402a0a3d9effe44
mh.mdhy <150346388+moamadmdh@users.noreply.github.com>
2024-02-02 09:08:32 +0330

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views
-from blog.views import user_signup, user_login, Bimar_dashboard
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', user_signup, name='user_signup'),
-    path('login/', user_login, name='login'),
-    path('Bimar_dashboard/', Bimar_dashboard, name='Bimar_dashboard'),
-    
+    path('', include(('authentication.urls', ''))),
+    path('login/', include(('authentication.urls', 'login/'))),
 ]
